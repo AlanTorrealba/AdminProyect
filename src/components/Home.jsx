b4b21ebde8b26ececb88ba7fba35853c1e5404c1
ant <antorrealbatapia@gamil.com>
2023-10-17 23:41:22 -0400

    Avance de pagina con tailwind

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+
+// const people = [
+//   {
+//     name: 'Calvin Hawkins',
+//     email: 'calvin.hawkins@example.com',
+//     image:
+//       'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//   },
+//   {
+//     name: 'Kristen Ramos',
+//     email: 'kristen.ramos@example.com',
+//     image:
+//       'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//   },
+//   {
+//     name: 'Ted Fox',
+//     email: 'ted.fox@example.com',
+//     image:
+//       'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//   },
+// ]
 
 function Home() {
   return (
-    <div>Home</div>
-  )
-}
+    //   <ul className="divide-y divide-gray-200">
+    //   {people.map((person) => (
+    //     <li key={person.email} className="py-4 flex">
+    //       <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+    //       <div className="ml-3">
+    //         <p className="text-sm font-medium text-gray-900">{person.name}</p>
+    //         <p className="text-sm text-gray-500">{person.email}</p>
+    //       </div>
+    //     </li>
+    //   ))}
+    // </ul>
 
-export default Home
+    <div>
+      <div className="grid grid-cols-1 w-full">
+        <div className="bg-red-500">A</div>
+      </div>
+      <div className="grid grid-cols-2 gap-1">
+      <div className="bg-cyan-500 h-screen w-56 m-0">b</div>
+      <div className="bg-white h-screen m-0">conten</div>
+      </div>
+    </div>
+  );
+}
+export default Home;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,23 +30,47 @@ function Login() {
   };
 
   return (
-    <div className="formLogin">
-      <h1>Login</h1>
-      <div>
-        <form action="" className="formBody" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Usuario"
-            onChange={handleChangeUser}
-          />
-          <input
-            type="password"
-            placeholder="Contraseña"
-            onChange={handleChangePassword}
-          />
-          <button>Iniciar</button>
+    <div className="flex items-center justify-center min-h-screen backdrop-blur-sm  ">
+
+    <div className="flex min-h-lg w-96  flex-col justify-center px-6 py-12 lg:px-8 rounded-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,0.8)] bg-gradient-to-r from-cyan-500 to-blue-500 ">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-white">
+          Login
+        </h1>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="" className="space-y-6" onSubmit={handleSubmit}>
+          <div className="mt-2">
+            <input
+              className="appearance-none bg-transparent border-b-2 w-full text-white mr-3 placeholder:text-white py-1 px-2 leading-tight focus:outline-none" 
+              type="text"
+              placeholder="Usuario"
+              onChange={handleChangeUser}
+            />
+          </div>
+
+          <div className="mt-2">
+            <input
+              className="appearance-none bg-transparent border-b-2 w-full text-white mr-3 placeholder:text-white py-1 px-2 leading-tight focus:outline-none "
+              type="password"
+              placeholder="Contraseña"
+              onChange={handleChangePassword}
+            />
+          </div>
+          
+
+          <div className="">
+            <button className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-black font-bold shadow-sm hover:bg-emerald-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600  transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none ">
+              Iniciar
+            </button>
+            
+  
+         
+          </div>
         </form>
       </div>
+    </div>
     </div>
   );
 }
