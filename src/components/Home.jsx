f1cb1bb7f3d03f20ccc8d62b07e1860f5c40d575
Ant <antorrealbatapia@gmail.com>
2025-09-07 20:52:09 -0400

    dashboard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import CardBar from "./CardBar";
 import Tabla from "./Tabla";
-
+import Dashboard from "./Dashboard";
+import DashboardTanques from "./DashboardTanques";
 function Home() {
   return (
     <div className="flex flex-row flex-wrap ml-10 justify-center items-center">
-      <div className="flex flex-wrap overflow-hidden">
-        {/* <CardBar valor="100" tiempo="100" /> */}
-        {/* <CardBar valor="100" tiempo="200" />
-        <CardBar valor="100" tiempo="300" /> */}
-      </div>
-      <div className="m-10 w-full">
-        {/* <Tabla  /> */}
-      </div>
+      {/* <div className="flex flex-wrap overflow-hidden">
+        <CardBar valor="100" tiempo="100" />
+        <CardBar valor="100" tiempo="200" />
+        <CardBar valor="100" tiempo="300" />
+      </div> */}
+      {/* <div className="m-10 w-full">
+        <Tabla  />
+      </div> */}
+      <DashboardTanques/>
+      <Dashboard />
     </div>
   );
 }
